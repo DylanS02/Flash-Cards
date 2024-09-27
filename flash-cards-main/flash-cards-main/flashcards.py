41640da5ef0944b8f646133e03b417ddbe4212c5
DylanS02 <156218888+DylanS02@users.noreply.github.com>
2024-09-27 14:55:45 -0400

    Add files via upload

---
+++ flash-cards-main/flash-cards-main/flashcards.py
@@ -0,0 +1,360 @@
+alabama = input("Capital of Alabama: ")
+if alabama == "montgomery":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		alabama = input("Capital of Alabama: ")
+		if alabama == "montgomery":
+			print("Correct")
+			i = 1
+	
+alaska = input("Capital of Alaska: ")
+if alaska == "juneau":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		alaska = input("Capital of Alaska: ")
+		if alaska == "juneau":
+			print("Correct")
+			i = 1
+	
+arizona = input("Capital of Arizona: ")
+if arizona == "phoenix":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		arizona = input("Capital of Arizona: ")
+		if arizona == "phoenix":
+			print("Correct")
+			i = 1
+
+arkansas = input("Capital of Arkansas: ")
+if arkansas == "little rock":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		arkansas = input("Capital of Arkansas: ")
+		if arkansas== "little rock":
+			print("Correct")
+			i = 1
+
+delaware = input("Capital of Delaware: ")
+if delaware == "dover":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		delaware = input("Capital of Delaware: ")
+		if delaware == "dover":
+			print("Correct")
+			i = 1
+	
+california = input("Capital of California: ")
+if california == "sacramento":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		california = input("Capital of California: ")
+		if california == "sacramento":
+			print("Correct")
+			i = 1
+			
+colorado = input("Capital of Colorado: ")
+if colorado == "denver":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		colorado = input("Capital of Colorado: ")
+		if colorado == "denver":
+			print("Correct")
+			i = 1
+
+connecticut = input("Capital of Connecticut: ")
+if connecticut == "hartford":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		connecticut = input("Capital of Connecticut: ")
+		if connecticut == "hartford":
+			print("Correct")
+			i = 1
+	
+florida = input("Capital of Florida: ")
+if florida == "tallahassee":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		florida = input("Capital of Florida: ")
+		if florida == "tallahassee":
+			print("Correct")
+			i = 1
+
+georgia = input("Capital of Georgia: ")
+if georgia == "atlanta":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		georgia = input("Capital of Georgia: ")
+		if georgia == "atlanta":
+			print("Correct")
+			i = 1
+
+hawaii = input("Capital of Hawaii: ")
+if hawaii == "honolulu":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		hawaii = input("Capital of Hawaii: ")
+		if hawaii == "honolulu":
+			print("Correct")
+			i = 1
+
+maine = input("Capital of Maine: ")
+if maine == "augusta":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		maine = input("Capital of Maine: ")
+		if maine == "augusta":
+			print("Correct")
+			i = 1
+
+
+michigan = input("Capital of Michigan: ")
+if michigan == "lansing":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		michigan = input("Capital of Michigan: ")
+		if michigan == "lansing":
+			print("Correct")
+			i = 1
+
+north_dakota = input("Capital of North Dakota: ")
+if north_dakota == "bismarck":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		north_dakota = input("Capital of North Dakota: ")
+		if north_dakota == "bismarck":
+			print("Correct")
+			i = 1
+
+ohio = input("Capital of Ohio: ")
+if ohio == "columbus":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		ohio = input("Capital of Ohio: ")
+		if ohio == "columbus":
+			print("Correct")
+			i = 1
+
+oklahoma = input("Capital of Oklahoma: ")
+if oklahoma == "oklahoma city":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		oklahoma = input("Capital of Oklahoma: ")
+		if oklahoma == "oklahoma city":
+			print("Correct")
+			i = 1
+
+oregon = input("Capital of Oregon: ")
+if oregon == "salem":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		oregon = input("Capital of Oregon: ")
+		if oregon == "salem":
+			print("Correct")
+			i = 1
+
+pennsylvania = input("Capital of Pennsylvania: ")
+if pennsylvania == "harrisburg":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		pennsylvania = input("Capital of Pennsylvania: ")
+		if pennsylvania == "harrisburg":
+			print("Correct")
+			i = 1
+
+rhode_island = input("Capital of Rhode Island: ")
+if rhode_island == "providence":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		rhode_island = input("Capital of Rhode Island: ")
+		if rhode_island == "providence":
+			print("Correct")
+			i = 1
+
+south_carolina = input("Capital of South Carolina: ")
+if south_carolina == "columbia":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		south_carolina = input("Capital of South Carolina: ")
+		if south_carolina == "columbia":
+			print("Correct")
+			i = 1
+
+south_dakota = input("Capital of South Dakota: ")
+if south_dakota == "pierre":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		south_dakota = input("Capital of South Dakota: ")
+		if south_dakota == "pierre":
+			print("Correct")
+			i = 1
+
+tennessee = input("Capital of Tennessee: ")
+if tennessee == "nashville":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		tennessee = input("Capital of Tennessee: ")
+		if tennessee == "nashville":
+			print("Correct")
+			i = 1
+
+texas = input("Capital of Texas: ")
+if texas == "austin":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		texas = input("Capital of Texas: ")
+		if texas == "austin":
+			print("Correct")
+			i = 1
+
+utah = input("Capital of Utah: ")
+if utah == "salt lake city":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		utah = input("Capital of Utah: ")
+		if utah == "salt lake city":
+			print("Correct")
+			i = 1
+
+vermont = input("Capital of Vermont: ")
+if vermont == "montpelier":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		vermont = input("Capital of Vermont: ")
+		if vermont == "montpelier":
+			print("Correct")
+			i = 1
+
+virginia = input("Capital of Virginia: ")
+if virginia == "richmond":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		virginia = input("Capital of Virginia: ")
+		if virginia == "richmond":
+			print("Correct")
+			i = 1
+
+washington = input("Capital of Washington: ")
+if washington  == "olympia":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		washington = input("Capital of Washington: ")
+		if washington == "olympia":
+			print("Correct")
+			i = 1
+
+west_virginia = input("Capital of West Virginia: ")
+if west_virginia == "charleston":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 1
+	while i == 0:
+		west_virginia = input("Capital of West Virginia: ")
+		if west_virginia == "charleston":
+			print("Correct")
+			i = 1
+
+wisconsin = input("Capital of Wisconsin: ")
+if wisconsin == "madison":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		wisconsin = input("Capital of Wisconsin: ")
+		if wisconsin == "madison":
+			print("Correct")
+			i = 1
+
+wyoming = input("Capital of Wyoming: ")
+if wyoming == "cheyenne":
+	print("Correct")
+else:
+	print("Wrong")
+	i = 0
+	while i == 0:
+		wyoming = input("Capital of Wyoming: ")
+		if wyoming == "cheyenne":
+			print("Correct")
+			i = 1
